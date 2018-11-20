d9304920d0e76e058da2a141941447bf17fd5dc6
andooown <andooown.dev@gmail.com>
2018-11-20 17:17:52 +0900

    Create Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <Navbar />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
   components: {
+    Navbar,
     HelloWorld
   }
 };
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <b-navbar variant="primary" type="dark">
+        <b-navbar-brand>twitter-icon-classifier</b-navbar-brand>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
