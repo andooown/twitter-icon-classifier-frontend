cb8f1ca7909637f788d91acf5121be26c9ece749
andooown <andooown.dev@gmail.com>
2018-11-28 23:12:04 +0900

    Create router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router.js";
 import store from "./store.js";
 
 // Bootstrap
@@ -11,6 +12,7 @@ Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount("#app");
